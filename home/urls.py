c92f16523fb84a6d2b3ef49c299e0745a9404286
Buraiqu <buraiquanwar@gmail.com>
2022-11-30 13:37:47 +0530

    machinetest 2

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import  views
 app_name = 'home'
 
 urlpatterns = [
-    path('/home',views.home_linkedin, name = 'homepage'),
+    path('home',views.home_linkedin, name = 'homepage'),
     path('',views.home_login, name = 'login')
 ]
